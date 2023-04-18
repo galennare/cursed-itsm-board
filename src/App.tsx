3cdc8e19987aba7fd0dd790236120eb860868890
Alex <acroce@udel.edu>
2023-04-18 14:03:15 -0400

    Added name

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                    Alexandra Croce
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div>
+                <h1>Team 3</h1>
+                <h4>Galen Nare</h4>
+                <h4>Solenn Gacon</h4>
+                <h4>Nicholas DiGirolamo</h4>
+                <h4>Alexandra Croce</h4>
+            </div>
+        </header>
+    </div>
+  );
 }
 
 export default App;
