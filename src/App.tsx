0d983165f9efcb2a8c9c13349e0d1d1ac9cb1452
Alex <acroce@udel.edu>
2023-04-18 15:13:22 -0400

    made the header smaller and put all of our names on one line

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,10 @@ function App() {
             <header className="App-header">
                 <div>
                     <h1>Team 3</h1>
-                    <h4>Galen Nare</h4>
-                    <h4>Michael Arocho</h4>
-                    <h4>Solenn Gacon</h4>
-                    <h4>Nicholas DiGirolamo</h4>
-                    <h4>Andrew Roberts</h4>
-                    <h4>Alexandra Croce</h4>
+                    <h4>
+                        Galen Nare, Michael Arocho, Solenn Gacon, Nicholas
+                        DiGirolamo, Andrew Roberts, Alexandra Croce
+                    </h4>
                 </div>
             </header>
             <TicketButton></TicketButton>
