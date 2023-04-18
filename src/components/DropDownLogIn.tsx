33a764369634cacd1711173466a5f76e5cd59563
Alex <acroce@udel.edu>
2023-04-18 15:40:55 -0400

    Okay, learned something new. If you do not save every file individually, you will not be able to run the website

---
+++ src/components/DropDownLogIn.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+export function DropDownLogIn(): JSX.Element {
+    return (
+        <span>
+            <Button>Logged in as User</Button>
+        </span>
+    );
+}
