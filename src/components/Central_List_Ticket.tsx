a096da58daafaa3efa0a849ed2d01758aa61e1e4
Solenn Gacon <solgacon@udel.edu>
2023-04-23 01:21:05 -0400

    removed reference code and created the view

--- src/components/Central_List_Ticket.tsx
+++ src/components/Central_List_Ticket.tsx
@@ -7,9 +7,6 @@ import { User } from "../interface/User";
 export type StatusType = "pending" | "in progress" | "resolved";
 
 export function RepresentTicket(): JSX.Element {
-    //NOTES:
-    //Button to edit, button to copy, button to create new textbox
-    //Dropdown for the Type union options
     const current_date = new Date();
 
     //STATE
@@ -184,62 +181,8 @@ export function RepresentTicket(): JSX.Element {
             return <p>{imageNotInEditMode()}</p>;
         }
     }
-}
 
-//REFERENCE CODE: InEditMode
-export function EditMode(): JSX.Element {
-    //This is State
-    const [inEditMode, setInEditMode] = useState<boolean>(false);
-    const [userName, setUserName] = useState<string>("Your Name");
-    const [isStudent, setIsStudent] = useState<boolean>(true);
-
-    //This is Control
-    function updateInEditMode(event: React.ChangeEvent<HTMLInputElement>) {
-        setInEditMode(event.target.checked);
-    }
-
-    function updateUserName(event: React.ChangeEvent<HTMLInputElement>) {
-        setUserName(event.target.value);
-    }
-
-    function updateIsStudent(event: React.ChangeEvent<HTMLInputElement>) {
-        setIsStudent(event.target.checked);
-    }
-
-    function textNotInEditMode(): string {
-        if (isStudent === true) {
-            return userName + " is a student";
-        } else {
-            return userName + " is not a student";
-        }
-    }
-
-    function textInEditMode(): JSX.Element {
-        if (inEditMode === true) {
-            return (
-                <div>
-                    <Form.Check
-                        type="checkbox"
-                        id="is-student-check"
-                        label="Student?"
-                        checked={isStudent}
-                        onChange={updateIsStudent}
-                    />
-                    <Form.Group controlId="formStudentName">
-                        <Form.Label>Name:</Form.Label>
-                        <Form.Control
-                            value={userName}
-                            onChange={updateUserName}
-                        />
-                    </Form.Group>
-                </div>
-            );
-        } else {
-            return <p>{textNotInEditMode()}</p>;
-        }
-    }
-
-    //This is View
+    //VIEW
     return (
         <div>
             <Form.Switch
@@ -248,7 +191,10 @@ export function EditMode(): JSX.Element {
                 checked={inEditMode}
                 onChange={updateInEditMode}
             />
-            <div>{textInEditMode()}</div>
+            <div>{titleInEditMode()}</div>
+            <div>{descriptionInEditMode()}</div>
+            <div>{priorityInEditMode()}</div>
+            <div>{imageInEditMode()}</div>
         </div>
     );
 }
