dc0a8fdfc20adc7f8cfbacdcae46f99bf4a22a10
Alex <acroce@udel.edu>
2023-05-02 15:21:44 -0400

    Added test for NavigationBar

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders react link", () => {
-    render(<App />);
-    const linkElement = "a";
-    expect(linkElement).toBeInTheDocument();
-});
