8200dd410f77cdae75623e60441897bbf025fdda
MichaelBrandon-UDel <70721605+MichaelBrandon-UDel@users.noreply.github.com>
2023-05-01 23:32:19 -0400

    Profile Photo now adjustable, editing CSS

--- src/components/ProfilePhoto.tsx
+++ src/components/ProfilePhoto.tsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import defaultImage from "../default-profile.png";
 
-export function ProfilePhoto(): JSX.Element {
-    return <img src={defaultImage} alt="Default PFP" />;
+interface PFP {
+    searcher: string;
 }
+
+const ProfilePhoto: React.FC<PFP> = ({ searcher }) => {
+    const [userPhoto, setPhoto] = useState(searcher);
+
+    const photoInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const photo = event.target.files && event.target.files[0];
+
+        if (photo) {
+            const reader = new FileReader();
+
+            reader.onload = (readerEvent: ProgressEvent<FileReader>) => {
+                const query =
+                    readerEvent.target && (readerEvent.target?.result ?? "");
+                setPhoto(query as string);
+            };
+
+            reader.readAsDataURL(photo);
+        }
+    };
+
+    if (!searcher) {
+        return <img src={defaultImage} alt="Default PFP" />;
+    } else {
+        return (
+            <div className="App">
+                <img src={userPhoto} alt="USER_PFP" />;
+                <input type="file" onChange={photoInput} accept="image/*" />
+            </div>
+        );
+    }
+};
+
+/*
+export function ProfilePhoto(): JSX.Element {
+    return (
+        <div className="App">
+            <img src={defaultImage} alt="Default PFP" />;
+            <input type="file" onChange={photoInput} accept="image/*" />
+        </div>
+    );
+}
+*/
 
 export default ProfilePhoto;
