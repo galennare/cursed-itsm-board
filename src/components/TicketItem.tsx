c7cbba065670451844d198e3ba58413552d20a35
Solenn Gacon <solgacon@udel.edu>
2023-05-05 13:56:03 -0400

    Did last changes for interface import

--- src/components/TicketItem.tsx
+++ src/components/TicketItem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Hook } from "../TicketDatabase";
 import { useDrag } from "react-dnd";
-import { Ticket } from "../Interface/TicketInterface";
+import { Ticket } from "../interface/TicketInterface";
 
 /*
     KEY PART OF MVP #########################################################
