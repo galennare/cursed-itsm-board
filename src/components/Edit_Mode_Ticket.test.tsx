c5176081efdb73f94d68d6f2aa0d00dd922d2ef4
Solenn Gacon <solgacon@udel.edu>
2023-05-05 14:24:08 -0400

    added some more tests for EditMode test file

--- src/components/Edit_Mode_Ticket.test.tsx
+++ src/components/Edit_Mode_Ticket.test.tsx
@@ -62,13 +62,55 @@ test("Can switch into Edit Mode", () => {
     expect(screen.getByRole("textbox")).toBeInTheDocument();
     expect(screen.getAllByRole("checkbox")).toHaveLength(2);
 });
-test("Editing the title changes the text of the title", () => {
+test("Editing the title changes the title text", () => {
     const switchButton = screen.getByRole("checkbox");
     switchButton.click();
     const nameBox = screen.getByRole("textbox");
     userEvent.type(nameBox, "Computer problems");
     switchButton.click();
     expect(screen.getByText(/Computer problems/i)).toBeInTheDocument();
+});
+test("Editing the description changes the description text", () => {
+    const switchButton = screen.getByRole("checkbox");
+    switchButton.click();
+    const descriptionBox = screen.getByRole("textbox");
+    userEvent.type(descriptionBox, "This is the description of the ticket");
+    switchButton.click();
+    expect(
+        screen.getByText(/This is the description of the ticket/i)
+    ).toBeInTheDocument();
+});
+test("Editing the priority changes the priority text", () => {
+    const switchButton = screen.getByRole("checkbox");
+    switchButton.click();
+    const prioBox = screen.getByRole("textbox");
+    userEvent.type(prioBox, "5");
+    switchButton.click();
+    expect(screen.getByText(/5/i)).toBeInTheDocument();
+});
+test("Editing the image changes the image text", () => {
+    const switchButton = screen.getByRole("checkbox");
+    switchButton.click();
+    const imageBox = screen.getByRole("textbox");
+    userEvent.type(imageBox, "image.png");
+    switchButton.click();
+    expect(screen.getByText(/image.png/i)).toBeInTheDocument();
+});
+test("Editing the status changes the dropdown option", () => {
+    const switchButton = screen.getByRole("checkbox");
+    switchButton.click();
+    const statusBox = screen.getByRole("status");
+    userEvent.type(statusBox, "Pending");
+    switchButton.click();
+    expect(screen.getByText(/Pending/i)).toBeInTheDocument();
+});
+test("Editing the assignee changes the assignee text", () => {
+    const switchButton = screen.getByRole("checkbox");
+    switchButton.click();
+    const assignBox = screen.getByRole("textbox");
+    userEvent.type(assignBox, "Victor Leclercq");
+    switchButton.click();
+    expect(screen.getByText(/Victor Leclercq/i)).toBeInTheDocument();
 });
 /*test("Different name, click student box twice changes the text", () => {
     const switchButton = screen.getByRole("checkbox");
