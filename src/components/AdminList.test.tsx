47763551169a7d35b77e4a0a671be620be5f74d8
Andrew Roberts <andrew@azroberts.com>
2023-05-15 15:32:52 -0400

    Added test files for AdminList & TicketItemEditable

---
+++ src/components/AdminList.test.tsx
@@ -0,0 +1,50 @@
+import { render, screen } from "@testing-library/react";
+import { AdminList, canDrop } from "./AdminList";
+import React, { useState } from "react";
+import { Ticket } from "../Interface/TicketInterface";
+import { DndProvider, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { UserRole } from "./NavigationBar";
+
+test("TicketList is rendering.", () => {
+    const newTicket: Ticket = {
+        id: 1,
+        title: "Computer Issues",
+        description: "This is the description for ticket one.",
+        priority: 0,
+        last_modified: new Date(),
+        author: "Joe Biden",
+        status: "Pending",
+        assignee: "Nick DiGirolamo",
+        image_path: "path_to_image"
+    };
+
+    function TicketListHookWrapper(): JSX.Element {
+        const [list, setList] = useState<Ticket[]>([]);
+        return (
+            <DndProvider backend={HTML5Backend}>
+                <AdminList
+                    title={"test"}
+                    userRole={UserRole.Super}
+                    requiredRole={UserRole.Super}
+                    userList={list}
+                    adminList={list}
+                    centralList={list}
+                    setUserList={setList}
+                    setAdminList={setList}
+                    setCentralList={setList}
+                ></AdminList>
+            </DndProvider>
+        );
+    }
+
+    render(<TicketListHookWrapper />);
+    const ticketList = screen.getByRole("ticket_list");
+    expect(ticketList).toBeInTheDocument();
+});
+
+test("canDrop is working properly.", () => {
+    const userRole = UserRole.User;
+    const requiredRole = UserRole.Super;
+    expect(!canDrop(userRole, requiredRole)).toBeTruthy();
+});
---
+++ src/components/TicketItemEditable.test.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { screen, render } from "@testing-library/react";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+import { Ticket } from "../Interface/TicketInterface";
+import { TicketItemEditable } from "./TicketItemEditable";
+
+test("There is a TicketItem", () => {
+    const ticketAuthor = screen.getByText(/Author/i);
+    expect(ticketAuthor).toBeInTheDocument();
+});
+
+beforeEach(() => {
+    function ticketSetter(ticket: Ticket): void {
+        console.log(`'${ticket.title}' was set.`);
+    }
+
+    const newTicket: Ticket = {
+        id: 1,
+        title: "Computer Issues",
+        description: "This is the description for ticket one.",
+        priority: 0,
+        last_modified: new Date(),
+        author: "Joe Biden",
+        status: "Pending",
+        assignee: "Nick DiGirolamo",
+        image_path: "path_to_image"
+    };
+
+    function TicketItemHookWrapper(): JSX.Element {
+        return (
+            <DndProvider backend={HTML5Backend}>
+                <TicketItemEditable
+                    ticket={newTicket}
+                    ticketSetter={ticketSetter}
+                />
+            </DndProvider>
+        );
+    }
+
+    render(<TicketItemHookWrapper />);
+});
+
+test("There is a TicketItem", () => {
+    const ticketTitle = screen.getByText(/Title/i);
+    const ticketAuthor = screen.getByText(/Author/i);
+    const ticketAssignee = screen.getByText(/Assignee/i);
+    const ticketDescription = screen.getByText(/Description/i);
+
+    expect(ticketAuthor).toBeInTheDocument();
+    expect(ticketTitle).toBeInTheDocument();
+    expect(ticketAssignee).toBeInTheDocument();
+    expect(ticketDescription).toBeInTheDocument();
+});
