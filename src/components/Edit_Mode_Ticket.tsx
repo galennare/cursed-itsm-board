7b2ed269377a8b0ec398e21713baf33c8bc347bc
Solenn Gacon <solgacon@udel.edu>
2023-05-16 14:59:57 -0400

    edited priority, now it shows up correctly

--- src/components/Edit_Mode_Ticket.tsx
+++ src/components/Edit_Mode_Ticket.tsx
@@ -203,14 +203,12 @@ export function EditTicket({
                             value={ticketPriority}
                             onChange={updateTicketPriority}
                         >
-                            {priorityValues.map((priority: number) => (
-                                <option
-                                    key={ticketPriority}
-                                    value={ticketPriority}
-                                >
-                                    {ticketPriority}
-                                </option>
-                            ))}
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
                         </Form.Select>
                     </Form.Group>
                     <div>{ticketPriority}</div>
